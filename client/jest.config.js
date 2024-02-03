463906800570708a02c30acf0be45762de1975c4
John DeRusso <derussoj@oregonstate.edu>
2024-02-03 11:54:50 -0500

    Add citations

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,6 +1,11 @@
+/*
+code provided by ChatGPT4 via chat.openai.com
+
+Prompt(s): requested help eliminating errors after installing Jest
+*/
 module.exports = {
     testEnvironment: 'jsdom',
     moduleNameMapper: {
         '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js'
     }
-  };
+};
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,3 +1,8 @@
+/*
+code provided by ChatGPT4 via chat.openai.com
+
+Prompt(s): requested help creating a test file and placeholder test
+*/
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
