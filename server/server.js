58dd3ea685018369327276b1d54c8ad8bc281743
John DeRusso <derussoj@oregonstate.edu>
2024-02-04 18:43:11 -0500

    Initial authentication implementation

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 // Initialize Express app
 const app = express();
