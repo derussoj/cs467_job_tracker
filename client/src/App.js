9e644f3673e4384d858beb7142ef0374142fcaa9
John DeRusso <derussoj@oregonstate.edu>
2024-03-09 13:41:35 -0500

    Created tabbed dashboard display

--- client/src/App.js
+++ client/src/App.js
@@ -54,7 +54,7 @@ function App() {
         <NavBar currentUser={currentUser} onLogout={handleLogout} />
         <Routes>
           <Route path="/about" element={<AboutPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/dashboard" element={<DashboardPage backendUrl={backendUrl} currentUser={currentUser} />} />
           <Route path="/login" element={<LoginPage backendUrl={backendUrl} />} />
           <Route path="/" element={<HomePage />} />
           <Route path="/jobApplications" element={
